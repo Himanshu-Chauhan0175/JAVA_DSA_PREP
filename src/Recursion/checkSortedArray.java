e8b2f55e2518dc71087f47b190efae4838159a77
Himanshu Chauhan <himanshu.chauhan0175@gmail.com>
2025-08-19 20:10:59 +0530

    19Aug2025

---
+++ src/Recursion/checkSortedArray.java
@@ -0,0 +1,42 @@
+package Recursion;
+import java.util.Scanner;
+
+
+public class checkSortedArray {
+    public static boolean checkSort(int arr[],int index){
+        if(index==arr.length-1){
+         return true;
+        }
+      if(arr[index]<arr[index+1]){
+         return checkSort(arr,index+1);
+      }
+      else{
+          return false;
+      }
+    }
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+
+        if(checkSort(arr,0)){
+            System.out.println("Array is sorted.");
+        }
+        else{
+            System.out.println("Array is not sorted.");
+        }
+
+    }
+}
